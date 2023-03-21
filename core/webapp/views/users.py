985b286ea3d25e3aab416ce5a883059b240949d4
Sulatn Roziyev <jjo152065@gmail.com>
2023-03-21 18:29:24 +0600

    Добавил файл с функциналом для добавления и удаления пользователя

---
+++ core/webapp/views/users.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import User
+from django.views.generic import FormView, DeleteView
+from webapp.models import Project
+from django.shortcuts import reverse
+from webapp.forms import UserProjectForm
+from webapp.views.template_views import GroupPermissionMixin
+
+
+class CreateUser(GroupPermissionMixin, FormView):
+    template_name = 'add_user.html'
+    model = Project
+    form_class = UserProjectForm
+    groups = ['manager', 'lead']
+
+    def post(self, request, *args, **kwargs):
+        form = UserProjectForm(request.POST)
+        project = Project.objects.get(pk=kwargs.get('pk'))
+        if form.is_valid():
+            user = User.objects.get(pk=form.cleaned_data.get('users').pk)
+            project.users.add(user)
+        return super().post(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse('detail_project', kwargs={'pk': self.kwargs.get('pk')})
+
+
+class DeleteUser(GroupPermissionMixin, DeleteView):
+    model = User
+    success_url = '/'
+    groups = ['manager', 'lead']
+
+    #Сделал удаление вообще из базы
+    def get(self, request, *args, **kwargs):
+        remove = self.delete(request)
+        return remove
